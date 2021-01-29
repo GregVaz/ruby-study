acec497a7e3599963e0e8fd4282fdb82b35b9172
Gregorio <grvazquezya@ittepic.edu.mx>
2021-01-29 10:26:58 -0700

    Classes working with other classes

---
+++ classes_working/book_in_stock.rb
@@ -0,0 +1,71 @@
+class BookInStock
+
+  attr_reader :isbn, :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
